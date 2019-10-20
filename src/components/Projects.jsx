856b4962ba50bd18fbcb6d5849e35d9291cc5094
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-19 18:06:27 -0700

    set up p4 gif

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,10 @@ const Projects = () => {
         <div className="projects">
             <a name="projects--tag"></a>
             <div className="projects--title">Projects</div>
-            <div className="projects--p4">projects 4</div>
+            <div className="projects--p4">
+            </div>
+            <img className="projects--p4--img" src="https://media.giphy.com/media/Ih6uczYa6PcnrK3Duz/giphy.gif" alt="movieguy"/>
+
             <div className="projects--p2">projects 2</div>
             <div className="projects--p3">projects 3</div>
             <div className="projects--p1">projects 1</div>
