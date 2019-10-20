685a1b57410b15f917e169d218b838a6110af22c
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-19 18:14:31 -0700

    complete p4

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,20 @@ const Projects = () => {
             <a name="projects--tag"></a>
             <div className="projects--title">Projects</div>
             <div className="projects--p4">
+                <h1>MovieGuy</h1>
+                <ul>
+                    <li>
+                    Offer movie search and exploration for user, user can save movies as favourite.
+                    </li>
+                    <li>
+                    A MERN-stack web application, developed with JavaScript, MongoDB, Express, React and Node, Sass. * Deployed project in Heroku.
+                    </li>
+                    <li>
+                    Used animations to improve user experience.
+                    </li>
+                </ul>
+                <a href="#">Open source on GitHub</a>
+                <a href="#">Launch Site</a>
             </div>
             <img className="projects--p4--img" src="https://media.giphy.com/media/Ih6uczYa6PcnrK3Duz/giphy.gif" alt="movieguy"/>
 
