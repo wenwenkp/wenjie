34a79706f0b87ceead0d9b7151626a5c8df0c14a
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-22 21:46:43 -0700

    deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wenjie/precache-manifest.ebc5316ead919781d9715d2633a1f14c.js"
+  "/wenjie/precache-manifest.0871f0eb2102e05f5f04c956af2e79de.js"
 );
 
 self.addEventListener('message', (event) => {
